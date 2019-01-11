0881b3e68814cf3247d1ac37194995f418dca70e
Ashrene Roy <ashrene_roy@mckinsey.com>
2019-01-11 21:31:57 +0530

    javascripting exercises

--- javascripting/rounding-numbers.js
+++ javascripting/rounding-numbers.js
@@ -1,3 +1,9 @@
 var roundUp = 1.5;
 var rounded = Math.round(roundUp); //Math object, round() method
 console.log(rounded);
+
+var a = Math.ceil(roundUp);
+console.log(a);
+
+var b = Math.floor(roundUp);
+console.log(b);
