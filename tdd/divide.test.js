dfcf5f49206ebd1c71bc26421014ab7c23cc7ead
Ashrene Roy <ashrene_roy@mckinsey.com>
2019-01-11 21:46:17 +0530

    Test updated

--- tdd/divide.test.js
+++ tdd/divide.test.js
@@ -1,6 +1,9 @@
 const divide = require('./divide');
 
 test('divide should return quotient',function(){
-    expect(divide(2,2).toEqual(1));
+    expect(divide(2,2)).toEqual(1);
 });
-module.exports = divide
+
+test('divide should return quotient',function(){
+    expect(divide(2,0)).toEqual(Infinity);
+});
