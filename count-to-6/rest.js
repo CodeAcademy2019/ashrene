1a8aeded363c0948383983016d352c6df11d9fd7
Ashrene Roy <ashrene_roy@mckinsey.com>
2019-01-16 19:33:01 +0530

    Modified average function

--- count-to-6/rest.js
+++ count-to-6/rest.js
@@ -1,5 +1,8 @@
 module.exports = average = (...arr) => {
     // what goes here?
+    if(arr.length == 0){
+        return 0;
+    }
     var sum = arr.reduce((acc,currV)=>acc+currV,0);
     return sum/arr.length;
 };
