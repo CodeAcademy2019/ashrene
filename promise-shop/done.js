26154acd0f77e6eb19b7427afca538916610e24a
Ashrene Roy <ashrene_roy@mckinsey.com>
2019-01-22 09:03:00 +0530

    promise exercises

--- promise-shop/done.js
+++ promise-shop/done.js
@@ -1,8 +1,10 @@
+const output = [];
 alwaysThrows=()=>{
     throw Error('OH NOES');
 }
 iterate = (arg=1) => {
     console.log(arg);
+    output.push(arg);
     return arg+1;
 }
 
@@ -18,7 +20,9 @@ const promise = Promise.resolve(iterate())
     .then(iterate)
     .then(iterate)
     .catch(error => {
+        output.push(error.message)
         console.log(error.message)
+
     })
 
-module.exports = {alwaysThrows,iterate,promise}
+module.exports = {alwaysThrows,iterate,promise,output}
--- promise-shop/done.test.js
+++ promise-shop/done.test.js
@@ -1,7 +1,19 @@
 count = require('./done')
 
 describe('count',() => {
-    test('should throw error "OH NOES"',() => {
+    it('should throw error "OH NOES"',() => {
         expect(count.alwaysThrows).toThrowError()
+    })
+})
+
+describe('Output',() => {
+    it('Should store the resolved value of promise till rejects',() => {
+        expect(count.output).toEqual([1, 2, 3, 4, 5, "OH NOES"])
+    })
+})
+
+describe('iterate',() => {
+    it('should increment argument by one', () => {
+        expect(count.iterate(1)).toEqual(2)
     })
 })
