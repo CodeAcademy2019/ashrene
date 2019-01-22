ba0d35be1929767d76c65672093b9fa4b8c42ebc
Ashrene Roy <ashrene_roy@mckinsey.com>
2019-01-22 11:56:18 +0530

    Linting done

--- assignment-1/bowling.js
+++ assignment-1/bowling.js
@@ -1,54 +1,45 @@
-// const roll = (pins) => {
-//     return pins;
-// }
-const score = (scoreArray) => {
-    return scoreArray.reduce((acc,currV) => (
-        acc += currV
-    ),0);
-}
+const score = scoreArray => (
+    scoreArray.reduce((acc, currV) => {
+        acc += currV;
+        return acc;
+    }, 0)
+)
 
 const pins = (input) => {
     const length = input.length;
-    let scoreArray = [];
-    for(let i = 0; i< length; i++){
-
-        if(input[i]<10){ //open
+    const scoreArray = [];
+    for (let i = 0; i < length; i += 1) {
+        // open
+        if (input[i] < 10) {
             scoreArray[i] = input[i];
-            console.log(i + ' ' + scoreArray[i] + ' open input ' + input[i] );
+            // console.log(i + ' ' + scoreArray[i] + ' open input ' + input[i] );
         }
-
-        if((input[i] < 10)&&(input[i]+input[i-1] == 10)&&(i-1>=0)&&(i<18)){ //spare
-            scoreArray[i] = 10 + input[i+1];
-            scoreArray[i-1] = 0;
-            console.log(i + ' ' + scoreArray[i] + ' spare input ' + input[i] );
+        if ((input[i] < 10) && (input[i] + input[i - 1] === 10) && (i - 1 >= 0) 
+        && (i < 18)) { // spare
+            scoreArray[i] = 10 + input[i + 1];
+            scoreArray[i - 1] = 0;
+            // console.log(i + ' ' + scoreArray[i] + ' spare input ' + input[i] );
         }
-        
-        if(input[i] == 10){ //strike
-            if(i<18){
-                if(input[i+1] == 10){
-                    scoreArray[i] = 10 + input[i+1] + input[i+2];
-                    console.log(i + ' ' + scoreArray[i] + ' strike input ' + input[i] );
-                }
-                // else{
-                //     score[i] = 10 + input[i+1] + input[i+2]
-                //     console.log(i + ' ' + score[i] + ' strike input ' + input[i] );
-                // }
-            }
-            if(i==18){
-                if((input[i] == 10)&&(input[i+1]<10)){
-                    scoreArray[i] = 10;
-                    scoreArray[i+1] = 10;
-                    console.log(i + ' ' + scoreArray[i] + ' strike input ' + input[i] );
-
-                }
-                if((input[i] == 10)&&(input[i+1]==10)){
-                    scoreArray[i] = 10;
-                    scoreArray[i+1] = 10;
-                    scoreArray[i+2] = 10;
-                    console.log(i + ' ' + scoreArray[i] + ' strike input ' + input[i] );
+        if (input[i] === 10) { // strike
+            if (i < 18) {
+                if (input[i + 1] === 10) {
+                    scoreArray[i] = 10 + input[i + 1] + input[i + 2];
+                    // console.log(i + ' ' + scoreArray[i] + ' strike input ' + input[i] );
                 }
             }
-
+            if (i === 18) {
+                if ((input[i] === 10) && (input[i + 1] < 10)) {
+                    scoreArray[i] = 10;
+                    scoreArray[i + 1] = 10;
+                    // console.log(i + ' ' + scoreArray[i] + ' strike input ' + input[i] );
+                }
+                if ((input[i] === 10) && (input[i + 1] === 10)) {
+                    scoreArray[i] = 10;
+                    scoreArray[i + 1] = 10;
+                    scoreArray[i + 2] = 10;
+                    // console.log(i + ' ' + scoreArray[i] + ' strike input ' + input[i] );
+                }
+            }
         }
     }
     return score(scoreArray);
--- assignment-1/bowling.test.js
+++ assignment-1/bowling.test.js
@@ -1,21 +1,23 @@
 const bowling = require('./bowling.js');
-const scoreArray = [3,4,5,6,1,2,3,4,5,10]
+
+const scoreArray = [3, 4, 5, 6, 1, 2, 3, 4, 5, 10];
 describe('pins', () => {
-    it('should give the score of the game', () => {
+    it('should give the score of the game - all open', () => {
         expect(bowling.pins([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toEqual(90);
     });
-    it('should give the score of the game', () => {
+    it('should give the score of the game - spare case', () => {
         expect(bowling.pins([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).toEqual(16);
     });
-    it('should give the score of the game', () => {
+    it('should give the score of the game - special 10th frame', () => {
         expect(bowling.pins([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10])).toEqual(30);
     });
 });
 
 describe('score', () => {
     it('should give the total sum of scores', () => {
-        expect(bowling.score(scoreArray)).toEqual(scoreArray.reduce((acc, currV) => (
-            acc += currV
-        ),0));
+        expect(bowling.score(scoreArray)).toEqual(scoreArray.reduce((acc, currV) => {
+            acc += currV;
+            return acc;
+        }, 0));
     });
 });
